b338711516f537e12b3ad22724b5a2db19474e19
Rosario-Astorga <rosarioastorgap@gmail.com>
2021-11-23 02:37:22 -0300

    Entrega examen final
    Estimada profesora y ayudantes, primero disculparme por la entrega atrasada del examen final del curso y, segundo, consultar si es posible que me la puedan recibir con el debido descuento debido a que tuve algunos problemas con entregarla a la hora. Entiendo completamente que es mi responsabilidad cumplir con los horarios establecidos y me disculpo por no cumplir en esta ocasión.
    Quedo atenta, saludos.

--- R/datos-proc.R
+++ R/datos-proc.R
@@ -7,7 +7,6 @@
 pacman::p_load(tidyverse, #Universo de paquetes
                sjmisc, #Para explorar datos
                dplyr, #Para manipular datos
-               sjlabelled, #Para trabajar con datos etiquetados
                haven, #cargar y exportar bases de datos en formatos .sav y .dta
                car) #Para recodificar manteniendo las etiquetas
 
@@ -31,55 +30,54 @@ head(MOVID_2020) # muestra las primeras filas presentes en el marco de datos
 
 frq(MOVID_2020$c2_3)
 frq(MOVID_2020$f2)
+frq(MOVID_2020$f1)
 frq(MOVID_2020$g1)
 frq(MOVID_2020$edad)
 frq(MOVID_2020$sexo)
-frq(MOVID_2020$g48)
+
 
 
 # 4. Selección de variables  -------------------------------------------
 
 ## Seleccionamos las siguientes variables:
 # - `factor_expansion`: Factor de expansión
-# - `id_encuesta`: Identificador del hogar
 # - `entrevistado`: Entrevistado seleccionado
 # - `c2_3`: Últimas 2 semanas: Se ha sentido bajoneado, deprimido, irritable o desesperanzado
 # - `f1`: Tiempo dedicado a las tareas domésticas en comparación a la situación previa al COVID-19
 # - `f2`: Tiempo dedicado al cuidado de personas en comparación a la situación previa al COVID-19
-# - `g48`: Tramo de ingreso total de su hogar el mes de noviembre de 2020
 # - `g1`: Durante la semana pasada, ¿trabajó al menos una hora, sin considerar los quehaceres domésticos?
 # - `edad`: Edad de los entrevistados
 # - `sexo`: Sexo de los entrevistados
 
 ## 4.1 MOVID-IMPACT 2020
 
-movid <- select(MOVID_2020, factor_expansion, id_encuesta, entrevistado, estado_sm = c2_3, variacion_td = f2, 
-                variacion_cuidados = f2, trabajo_productivo = g1, ingresos = g48, sexo, edad) 
+movid <- select(MOVID_2020, factor_expansion, entrevistado, salud_mental = c2_3, trabajo_domestico = f2, 
+                trabajo_productivo = g1, trabajo_cuidados = f1, sexo, edad) 
 
 # 5. Transformación de las variables ------------------------------------------------
 
+
 ## 5.1 Recodificación y codificación como NA
 
-### Primero, se recodifican las variables y se codifican los casos perdidos como NA
+#Primero, recodificamos las variables y codificamos los casos perdidos como NA
 
 movid_proc <- movid %>% 
+  mutate_at(vars(sexo, trabajo_domestico, salud_mental, trabajo_cuidados), funs(as.numeric(.))) %>% 
   filter(entrevistado == 1) %>%
-  mutate_at(vars(sexo, variacion_td, variacion_cuidados, ingresos), funs(as.numeric(.))) %>% 
-  mutate(estado_sm = car::recode(.$estado_sm, recodes = c("c(1)=0; c(2,3,4)=1; c(8,9)=NA"), as.factor = T, levels = c(0, 1)),
-         sexo = car::recode(.$sexo, c("1 = 'Hombre'; 2 = 'Mujer'"), as.factor = T,  levels = c('Hombre', 'Mujer')),
+  mutate(salud_mental = car::recode(.$salud_mental, recodes = c("1='No se ha sentido deprimido'; c(2,3,4)='Si se ha sentido deprimido'; c(8,9)=NA"), as.factor = T, levels = c('No se ha sentido deprimido', 'Si se ha sentido deprimido')),
+         sexo = car::recode(.$sexo, recodes = c("1 = 'Hombre'; 2 = 'Mujer'"), as.factor = T,  levels = c('Hombre', 'Mujer')),
+         trabajo_productivo = car::recode(.$trabajo_productivo, recodes = c("1 = 'Sí trabajó'; 2 = 'No trabajó'"), as.factor = T,  levels = c('Hombre', 'Mujer')),
          edad = car::recode(.$edad, recodes = c("18:39='Joven';40:60='Adulto'; 61:hi='Adulto mayor'"),
                             as.factor = T, levels = c("Joven", "Adulto", "Adulto mayor")),
-         variacion_td = car::recode(.$variacion_td, recodes = "c(1,2)=0; 3=1; c(8,9)= NA"),
-         variacion_cuidados = car::recode(.$variacion_cuidados, recodes = "c(1,2)=0; 3=1; c(8,9)= NA"),
-         ingresos = car::recode(.$ingresos, recodes = c("c(1,2,3)='Bajo'; c(4,5) = 'Medio'; c(6,7,8) = 'Alto'; c(98,99)=NA"),  as.factor = T, levels = c("Bajo", "Medio", "Alto"))) %>%
-  mutate_at(vars(variacion_td,trabajo_productivo, variacion_cuidados), funs(forcats::as_factor(.)))#Transformo las variables en un factor
-#para que mi modelo se estime de manera correcta, conservando la etiqueta de la variable y poder así saber la cat. de referencia
-
-
+         trabajo_cuidados = car::recode(.$trabajo_cuidados, recodes = c("c(1,2)='Se mantuvo o disminuyó trabajo cuidados'; 3='Aumentó trabajo cuidados'; c(8,9)= NA"), levels = c("No sabe/No responde", "Se mantuvo o disminuyó", "Aumentó")),
+         trabajo_domestico = car::recode(.$trabajo_domestico, recodes = c("c(1,2)='Se mantuvo o disminuyó trabajo doméstico'; 3='Aumentó trabajo doméstico'; c(8,9)= NA"), levels = c("No sabe/No responde", "Se mantuvo o disminuyó", "Aumentó"))) %>%
+  mutate_at(vars(edad, sexo, trabajo_domestico, trabajo_productivo, trabajo_cuidados), funs(forcats::as_factor(.)))#Transformo las variables en un factor para que mi modelo se estime de manera correcta, 
+#conservando la etiqueta de la variable y poder así saber la cat. de referencia.
 
 # 6. Visualización el set de datos -------------------------------------------
 
 head(movid_proc)#visualizamos el único set de datos movid_proc para revisar que todo esté en orden antes de guardar y exportar
+
 
 # 7. Guardar y exportar los datos ----------------------------------------
 
